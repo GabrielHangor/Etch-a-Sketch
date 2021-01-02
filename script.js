b10c6d966ced903ad3cf7e85cb9369df612d5f14
HangorG <gaba93@yandex.ru>
2021-01-03 02:56:12 +0300

    functionality to dynamically change the grid size

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const gridContainer = document.querySelector(".grid-container");
 const resetBtn = document.querySelector(".resetBtn");
+const gridSizeInput = document.querySelector("#size");
+const gridSizeEl = document.querySelector(".grid-size");
 
 // Create a whiteboard based on the ammount of rows & columns
 function createGrid(rows, columns) {
@@ -13,20 +15,31 @@ function createGrid(rows, columns) {
   }
 }
 
+function updateGrid(cellSize) {
+  gridContainer.textContent = "";
+  resetBoard();
+  createGrid(cellSize, cellSize);
+  gridSizeEl.textContent = `Grid Size: ${cellSize}x${cellSize}`;
+}
 
 // Reset the background color of all the cells to default
 function resetBoard() {
-  let cells = [...gridContainer.children];
+  const cells = [...gridContainer.children];
   cells.forEach((cell) => {
-    cell.style.backgroundColor = 'whitesmoke';
+    cell.style.backgroundColor = "whitesmoke";
   });
 }
-
 
 // Eventlisteners
 gridContainer.addEventListener("mouseover", (e) => {
   e.target.style.backgroundColor = "black";
 });
+
 resetBtn.addEventListener("click", resetBoard);
+
+gridSizeInput.addEventListener("input", () => {
+  let currentCellSize = gridSizeInput.value;
+  updateGrid(currentCellSize);
+});
 
 createGrid(10, 10);
